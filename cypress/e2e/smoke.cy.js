bd315175238a2b8590efb095fde9f4b354f06924
FlavioOliveiraNeto <flavio.neto@goias.gov.br>
2026-02-26 16:44:46 -0300

    First commit

--- cypress/e2e/smoke.cy.js
+++
@@ -1,7 +0,0 @@
-describe("Public landing page", () => {
-  it("renders the hero section", () => {
-    cy.visit("/");
-    cy.contains("Crie jornadas terapêuticas").should("be.visible");
-    cy.contains("Começar agora").should("be.visible");
-  });
-});
